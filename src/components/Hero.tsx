bd3e3a95afd170e30144f725f819777f1e0dac75
Euljehoon <nubddak2@gmail.com>
2026-07-28 21:41:08 -0500

    Updated resume

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -30,7 +30,7 @@ export default function Hero() {
               GitHub
             </span>
           </a>
-          <a href = "https://drive.google.com/file/d/1_Wls9nsmHeh1XcbGPa1Lw_DH_9rGTN9L/view?usp=sharing" download target = "_blank" rel = "noopener noreferrer" className="group relative">
+          <a href = "https://drive.google.com/file/d/1fPhoUsS6FfZ5JaaaU2tSdcBPNPlnyiyF/view?usp=sharing" download target = "_blank" rel = "noopener noreferrer" className="group relative">
             <Button className="bg-black hover:bg-[#FFCC33] text-white p-3 rounded-full">
               <FileText className="w-5 h-5" />
             </Button>
